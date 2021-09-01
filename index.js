3821c9fd8b6d0b7fa754444cdb72b78d2a436ba6
Pradhuman Demla <pradhumnd33@gmail.com>
2021-09-01 11:29:40 +0530

    initials

--- index.js
+++ index.js
@@ -16,8 +16,10 @@ const port = process.env.PORT || 4000
 
 
 // middleware
+
 app.use(cors());
 app.use("/assets",express.static(path.join(process.cwd(), "/public/assets")))
+app.use(express.static(path.join(process.cwd(), "/public/assets")))
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(helmet());
@@ -27,8 +29,6 @@ app.use(morgan("dev"));
 app.use("/auth", authRoute);
 app.use("/users", userRoute);
 app.use("/posts", postRoute);
-
-
 
 app.get('/',(req, res)=>{
     res.send("hello");
