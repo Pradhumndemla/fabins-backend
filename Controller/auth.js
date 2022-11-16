4a3e7dd0135a0be23f9ac359609eed061f6ca1cf
Pradhuman Demla <Pradhumnd33@gmail.com>
2022-11-16 07:10:30 +0530

    initials
    Signed-off-by: Pradhuman Demla <Pradhumnd33@gmail.com>

--- Controller/auth.js
+++ Controller/auth.js
@@ -5,28 +5,34 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const register = async (req, res) => {
-    let { username, email, password } = req.body
+    // let { username, email, password } = req.body
+    let { email, password } = req.body
     try {
         password = await bcrypt.hash(password, 4)
-        let old = await User.findOne({ $or: [{ username }, { email }] })
+        // let old = await User.findOne({ $or: [{ username }, { email }] })
+        let old = await User.findOne({ email })
         if (old) return res.status(401).send({ error: "invalid details" })
-        const user = await new User({ username, email, password })
+        // const user = await new User({ username, email, password })
+        const user = await new User({ email, password })
         await user.save();
-        return res.status(200).send({ success: "User Registered", user });
+        let token = await jwt.sign({ _id: user._id }, process.env.JWT_KEY)
+        return res.status(200).send({ success: "User Registered", token, user });
     } catch (error) {
-        return res.status(404).send({ error: `in catch bloack of api ${error} `  });
+        return res.status(404).send({ error: `in catch bloack of api ${error} ` });
     }
 }
 export const login = async (req, res) => {
-    let { username, email, password } = req.body
+    // let { username, email, password } = req.body
+    let { email, password } = req.body
     try {
-        const user = await User.findOne({ $or: [{ username }, { email }] });
+        // const user = await User.findOne({ $or: [{ username }, { email }] });
+        const user = await User.findOne({ email });
         if (!user) return res.status(401).send({ error: "user not found " });
         let checkpwd = await bcrypt.compare(password, user.password)
         if (!checkpwd) return res.status(400).send("wrong password");
-        // let token = await jwt.sign({_id:user._id},process.env.JWT_KEY)
-        return res.status(200).send({ success: "loged in ", user })
+        let token = await jwt.sign({ _id: user._id }, process.env.JWT_KEY)
+        return res.status(200).send({ success: "loged in ", token, user })
     } catch (error) {
-        return res.status(404).send({ error: `in catch bloack of api ${error} `  });
+        return res.status(404).send({ error: `in catch bloack of api ${error} ` });
     }
 }
